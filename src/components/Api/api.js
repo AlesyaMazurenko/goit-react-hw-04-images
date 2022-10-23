b1d64f236a34851e05be639ca4c3079679ad0f10
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-10-23 12:42:06 +0200

    Create api.js

---
+++ src/components/Api/api.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api';
+const KEY = '29288692-4de5473bca5fca6278995f222';
+const PAGE_LIMIT = '12';
+
+
+export const getImages = async (imagesName, page) => {
+    try {
+        const response = await axios.get(BASE_URL, {
+            params: {
+                key: KEY,
+                q: imagesName,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                per_page: PAGE_LIMIT,
+                page: page,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+    
+};
+
+
+
+// const instance = axios.create({
+//     baseURL: BASE_URL,
+//     params: {
+//         key: KEY,
+//         per_page: PAGE_LIMIT,
+//         orientation: 'horizontal',
+//         image_type: 'photo',
+//     },
+// });
