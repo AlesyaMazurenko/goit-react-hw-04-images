984c674e3c0a8cf1f196253aa89d31b1ff72d1f7
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-10-23 12:43:23 +0200

    create imageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,17 +1,18 @@
 import propTypes from 'prop-types';
 import './ImageGalleryItem.css';
 
-export const ImageGalleryItem = ({ images }) => {
+export const ImageGalleryItem = ({ images, onClick }) => {
     const elements = images.map(({ id, webformatURL, largeImageURL }) => {
-        return <li key={id} className='gallery-item'> 
-            <img src={webformatURL} alt="" />
-        </li>     
-    }) //onClick={() => onClick(({largeImageURL}))} 
-
+        return <li key={id} className='imageGalleryItem' onClick={() => onClick(({ id, largeImageURL }))} >
+            <img src={webformatURL} alt="" className='imageGalleryItem-image' />
+        </li>
+    }) 
     return elements;
-}
+};
+
+//export default ImageGalleryItem;
 
 ImageGalleryItem.propTypes = {
     images: propTypes.array,
-   // onClick: propTypes.func,
+   onClick: propTypes.func,
 }
