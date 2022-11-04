0881b89aed2431949a3aa464412b6688dcef265e
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-11-04 04:54:55 +0100

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,62 +1,111 @@
-import { Component } from "react";
+// import { Component } from "react";
+import { useState } from 'react';
 import './SearchBar.css';
 import { toast } from 'react-toastify';
 import propTypes from 'prop-types';
 import { ImSearch } from 'react-icons/im';
 
-export class SearchBar extends Component {
-    state = {
-        imagesName: '',
+export default function SearchBar({ onSubmit }) {
+    
+    const [imagesName, setImagesName] = useState('');
+
+    const changeHandler = event => {
+        setImagesName(event.target.value.toLowerCase());
     }
 
-    changeHandler = (event) => {
-         /*Универсальной метод сбора введённых данных */
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value.toLowerCase(),
-        })
-     }
-
-    submitHandler = event => {
+    const submitHandler = event => {
         event.preventDefault();
 
-        if (this.state.imagesName.trim() === '') {
+        if (imagesName.trim() === '') {
             toast.warning('Введите строку поиска');
             return;
         }
-            // передпсем ищ APP пропс             
-        this.props.onSubmit(this.state.imagesName); /*Передаём значение в App */
-        this.setState({ imagesName: '' });
+        // передпсем ищ APP пропс             
+        onSubmit(imagesName); /*Передаём значение в App */
+        setImagesName('');
     };
 
-    render() {
-        return (
-            <header className="searchbar">
-                <form className='searchForm' onSubmit={this.submitHandler}>
-                    <button type="submit" className="searchForm-button">
-                          <ImSearch size="24px" />
-                        <span className='searchForm-button-label'>Search</span>
-                    </button>
-                    <input
-                        className="searchForm-input"
-                        type="text"
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                        name="imagesName"
-                        value={this.state.imagesName}
-                        onChange={this.changeHandler}
-                    />
-                </form>
-            </header>
-        )
-    }
+    return (
+        <header className="searchbar">
+            <form className='searchForm' onSubmit={submitHandler}>
+                <button type="submit" className="searchForm-button">
+                    <ImSearch size="24px" />
+                    <span className='searchForm-button-label'>Search</span>
+                </button>
+                <input
+                    className="searchForm-input"
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                    name="imagesName"
+                    value={imagesName}
+                    onChange={changeHandler}
+                />
+            </form>
+        </header>
+    )
     
-}
+    
+};
+
+    // export default SearchBar;
 
 
 SearchBar.propTypes = {
     onSubmit: propTypes.func,
     // onChange: propTypes.func,
 }
+
+
+// export class SearchBar extends Component {
+//     state = {
+//         imagesName: '',
+//     }
+
+//     changeHandler = (event) => {
+//          /*Универсальной метод сбора введённых данных */
+//         const { name, value } = event.target;
+//         this.setState({
+//             [name]: value.toLowerCase(),
+//         })
+//      }
+
+//     submitHandler = event => {
+//         event.preventDefault();
+
+//         if (this.state.imagesName.trim() === '') {
+//             toast.warning('Введите строку поиска');
+//             return;
+//         }
+//             // передпсем ищ APP пропс             
+//         this.props.onSubmit(this.state.imagesName); /*Передаём значение в App */
+//         this.setState({ imagesName: '' });
+//     };
+
+//     render() {
+//         return (
+//             <header className="searchbar">
+//                 <form className='searchForm' onSubmit={this.submitHandler}>
+//                     <button type="submit" className="searchForm-button">
+//                           <ImSearch size="24px" />
+//                         <span className='searchForm-button-label'>Search</span>
+//                     </button>
+//                     <input
+//                         className="searchForm-input"
+//                         type="text"
+//                         autoComplete="off"
+//                         autoFocus
+//                         placeholder="Search images and photos"
+//                         name="imagesName"
+//                         value={this.state.imagesName}
+//                         onChange={this.changeHandler}
+//                     />
+//                 </form>
+//             </header>
+//         )
+//     }
+    
+// }
+
 
