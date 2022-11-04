edeac741d7ca2606c835a288d7c01f6692d997a3
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-11-04 04:54:50 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,50 +1,98 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { createPortal } from "react-dom";
 import propTypes from 'prop-types';
 import './Modal.css';
 
+const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-    componentDidMount() {
-        // console.log('componentDidMount');
-
-        window.addEventListener('keydown', this.handleKeyDown);
-    }
-
-    componentWillUnmount() {
-        // console.log('componentWillUnmount');
-        window.removeEventListener('keydown', this.handleKeyDown); 
-    }
-
-    handleKeyDown = e => {
-        if (e.code === 'Escape') {
-            // console.log('presssed ESC');
-            this.props.onClose();
+const Modal = ({ onClose, children }) => {
+        const handleOverlayClick = e => {
+  
+        if (e.currentTarget === e.target || e.code === "Escape")
+        {
+            console.log(e.code);
+            onClose();
         }
-    };
+        };
+    
+    useEffect(() => {
+        const onEscClick = e => {
+            if (e.code === "Escape") {
+                onClose();
+            }
+        };
+        
+        window.addEventListener('keydown', onEscClick);
 
-    handleOverlayClick = e => {
-        // console.log('click baskdop')
-        // console.log('e.currentTarget', e.currentTarget);
-        // console.log('e.target', e.target);
-        //e.currentTarget === e.target = проверяем что клик произощел только на сером фоне, а не на других єлементах overlay 
-        if (e.currentTarget === e.target) {
-            this.props.onClose();
-        }
-    }
+        return () => {
+            window.removeEventListener('keydown', onClose);
+        };
+    }, [onClose]);
 
-    render() {
-        return (
-            <div className="overlay" onClick={this.handleOverlayClick}>
+    // componentDidMount() {
+    //     window.addEventListener('keydown', this.handleKeyDown);
+    // }
+
+    // componentWillUnmount() {
+    //     window.removeEventListener('keydown', this.handleKeyDown); 
+    // }
+
+   
+
+
+        return createPortal(
+            <div className="overlay" onClick={handleOverlayClick}>
                 <div className="modal">
-                    {this.props.children}
+                    {children}
                    {/* // <img src="" alt="" /> */}
                 </div>
-            </div>
+            </div>,
+            modalRoot
             
         )
-    }
+    
 };
 
 Modal.propTypes = {
     onClose: propTypes.func,
 };
+
+export default Modal;
+
+
+
+// export default class Modal extends Component {
+//     componentDidMount() {
+//         window.addEventListener('keydown', this.handleKeyDown);
+//     }
+
+//     componentWillUnmount() {
+//         window.removeEventListener('keydown', this.handleKeyDown); 
+//     }
+
+//     handleKeyDown = e => {
+//         if (e.code === 'Escape') {
+           
+//             this.props.onClose();
+//         }
+//     };
+
+//     handleOverlayClick = e => {
+  
+//         if (e.currentTarget === e.target) {
+//             this.props.onClose();
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div className="overlay" onClick={this.handleOverlayClick}>
+//                 <div className="modal">
+//                     {this.props.children}
+//                    {/* // <img src="" alt="" /> */}
+//                 </div>
+//             </div>
+            
+//         )
+//     }
+// };
